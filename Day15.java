171622a7f7a9759ea56eb80fafcad8195bf59ec7
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-10 12:22:25 +0700

    Linked List

--- Day15.java
+++ Day15.java
@@ -4,11 +4,12 @@ import java.io.*;
 import java.util.*;
 
 // Bai tap ve cau truc du lieu LinkedList
-
+// Declare a class Node to define a singly linked list
 class Node {
     int data;
     Node next;
 
+    // Constructor for a Node
     Node(int d) {
         data = d;
         next = null;
@@ -16,10 +17,23 @@ class Node {
 }
 
 public class Day15 {
+    // insert() method to add a new Node
     public static Node insert(Node head, int data) {
-        // Complete this method
+        if (head == null) {
+            // Neu danh sach rong thi them 1 nut moi
+            head = new Node(data);
+        } else {
+            // nguoc lai thi duyet den nut cuoi cung va them nut moi
+            Node a = head; // Node a tro vao node dau tien cua danh sach
+            while (a.next != null) {
+                a = a.next;
+            }
+            a.next = new Node(data);
+        }
+        return head;
     }
 
+    // display() method to display data
     public static void display(Node head) {
         Node start = head;
         while (start != null) {
@@ -30,6 +44,8 @@ public class Day15 {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
+        // Define and init a Node
         Node head = null;
         int N = sc.nextInt();
 
