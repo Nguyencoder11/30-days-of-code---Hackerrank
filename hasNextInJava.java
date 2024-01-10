196c1b6eac129050026a6af66e3fac2f3bf82b82
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-10 10:57:09 +0700

    hasNext() method in Java

---
+++ hasNextInJava.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class hasNextInJava {
+    public static void main(String[] args) {
+        String path = "input.txt";
+        try {
+            File file = new File(path);
+
+            Scanner scan = new Scanner(file);
+
+            int lineNum = 1;
+
+            while (scan.hasNext()) {
+                String line = scan.nextLine();
+                System.out.println(lineNum++ + " " + line);
+            }
+            scan.close();
+        } catch (FileNotFoundException e) {
+            // Xử lý nếu không tìm thấy tệp tin
+            System.err.println("Không tìm thấy tệp tin: " + path);
+            e.printStackTrace();
+        }
+    }
+}
