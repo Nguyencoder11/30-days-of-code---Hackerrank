7faeef3a6f1caddd2185ef6e669397f22295dbb1
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-11 11:37:56 +0700

    Exceptions - String to Integer

---
+++ Day16.java
@@ -0,0 +1,24 @@
+// Day 16: Exceptions - String to Integer
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day16 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = bufferedReader.readLine();
+        try {
+            int n = Integer.parseInt(S);
+            System.out.println(n);
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+        bufferedReader.close();
+    }
+}
