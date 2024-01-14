cae9aad5b57e6dfb144a8fd917935e2ad040fada
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-14 15:05:09 +0700

    learn Queue

---
+++ dataStructure_Queue.java
@@ -0,0 +1,19 @@
+// Learn about data structure - Queue
+
+import java.io.*;
+import java.util.*;
+
+public class dataStructure_Queue {
+    private static Queue<Integer> queue1 = new LinkedList<>();
+    private static Queue<Integer> queue2 = new PriorityQueue<>();
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        scan.close();
+    }
+
+    static void show() {
+        
+    }
+}
