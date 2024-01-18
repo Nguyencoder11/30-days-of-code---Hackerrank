8c8528e79fabcb6a7c57ccf20ce8e650cea93649
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-18 17:10:55 +0700

    Operators

--- Day2.java
+++ Day2.java
@@ -1,12 +1,6 @@
 // Day 2: Operators
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 class Result {
     /*
