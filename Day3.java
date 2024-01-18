15adf244a2bad5c6336dfe3e3794ae15ceaa802f
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-18 17:12:45 +0700

    Intro to Conditional Statements

--- Day3.java
+++ Day3.java
@@ -1,12 +1,6 @@
 // Day 3: Intro to Conditional Statements
 
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class Day3 {
     public static void main(String[] args) throws IOException {
