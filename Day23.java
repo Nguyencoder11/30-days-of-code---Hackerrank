37a7b93f311ed2a225e0dac0ca448daac0f4b987
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-18 16:46:58 +0700

    BST Level-Order Traversal

---
+++ Day23.java
@@ -0,0 +1,62 @@
+// Day 23: BST Level-Order Traversal
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left, right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class Day23 {
+    static void levelOrder(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node node = queue.remove();
+            System.out.print(node.data + " ");
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node current;
+            if (data <= root.data) {
+                current = insert(root.left, data);
+                root.left = current;
+            } else {
+                current = insert(root.right, data);
+                root.right = current;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        Node root = null;
+
+        while (T-- > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        levelOrder(root);
+    }
+}
