40f27db09b235440cd02e665efca58f4a1a44b49
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-21 00:31:19 +0700

    Running Time and Complexity

--- Day25.java
+++ Day25.java
@@ -1,10 +1,32 @@
 // Day 25: Running Time and Complexity
 
-// import java.util.*;
-// import java.io.*;
+import java.util.*;
+import java.io.*;
 
 public class Day25 {
+    static boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+        if (n == 2 || n == 3)
+            return true;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        while (T-- > 0) {
+            int n = scan.nextInt();
+            if (isPrime(n)) {
+                System.out.println("Prime");
+            } else {
+                System.out.println("Not prime");
+            }
+        }
+        scan.close();
     }
 }
