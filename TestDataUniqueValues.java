00dcf9143cc89c95520e16c8d1959dc48b07d355
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-22 16:59:32 +0700

    class TestDataUniqueValues for Day27

--- TestDataUniqueValues.java
+++ TestDataUniqueValues.java
@@ -2,12 +2,10 @@
 public class TestDataUniqueValues {
 
     public static int[] get_array() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get_array'");
+        return new int[] { 1, 2 };
     }
 
     public static int get_expected_result() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get_expected_result'");
+        return 0;
     }
 }
