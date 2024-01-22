58be926a2f44ce3abe132db50901a3cf54034576
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-22 16:54:30 +0700

    class TestDataUniqueValues for Day27

---
+++ TestDataExactlyTwoDifferentMinimums.java
@@ -0,0 +1,13 @@
+
+public class TestDataExactlyTwoDifferentMinimums {
+
+    public static int[] get_array() {
+        return new int[0];
+    }
+
+    public static int get_expected_result() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get_expected_result'");
+    }
+
+}
