2581af214d675c96d87c3cef7f97a0213b7cb8d4
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-01-25 14:14:55 +0700

    Bitwise AND

--- Day29.java
+++ Day29.java
@@ -2,6 +2,15 @@
 
 import java.io.*;
 import java.util.stream.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 class Result {
 
@@ -16,7 +25,15 @@ class Result {
 
     public static int bitwiseAnd(int N, int K) {
         // Write your code here
-        int result;
+        int result = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = i + 1; j <= N; j++) {
+                int bitwiseAnd = i & j;
+                if (bitwiseAnd > result && bitwiseAnd < K) {
+                    result = bitwiseAnd;
+                }
+            }
+        }
 
         return result;
     }
@@ -26,7 +43,7 @@ class Result {
 public class Day29 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
 
         int t = Integer.parseInt(bufferedReader.readLine().trim());
 
